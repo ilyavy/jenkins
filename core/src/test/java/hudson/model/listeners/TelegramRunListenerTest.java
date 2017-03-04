7f63be65ae73fa886a73209db38fceead3e777ca
Ilnaz Fazliakhmetov <ilnaz94@gmail.com>
2017-03-04 13:55:39 +0300

    TelegramRunListener initial tests

---
+++ core/src/test/java/hudson/model/listeners/TelegramRunListenerTest.java
@@ -0,0 +1,96 @@
+package hudson.model.listeners;
+
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import ru.telegram.bot.TelegramApi;
+
+import java.io.PrintStream;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TelegramRunListenerTest {
+    @Mock
+    private TelegramApi telegramApi;
+
+    @Mock
+    private TaskListener taskListener;
+
+    @Mock
+    private PrintStream printStream;
+
+    @Mock
+    private Run r;
+
+    @InjectMocks
+    private TelegramRunListener<Run> telegramRunListener = new TelegramRunListener<>();
+
+    private final String displayName = "Test build";
+    private final String durationStr = "30 sec";
+
+    @Before
+    public void setUp() throws Exception {
+        when(taskListener.getLogger()).thenReturn(printStream);
+        when(r.getFullDisplayName()).thenReturn(displayName);
+        when(r.getDurationString()).thenReturn(durationStr);
+    }
+
+    @Test
+    public void onStartedSuccessTest() throws Exception {
+        String message = "Build " + r.getFullDisplayName() + " has started";
+        String notificationLog = "Telegram notification should have been sent";
+
+        telegramRunListener.onStarted(r, taskListener);
+
+        verify(telegramApi, times(1)).sendMessage(message);
+        verify(printStream, times(1)).println(notificationLog);
+    }
+
+    @Test
+    public void onCompletedSuccessTest() throws Exception {
+        String message = "Build " + r.getFullDisplayName() +
+                " has finished for " + r.getDurationString();
+        String notificationLog = "Telegram notification should have been sent";
+
+        telegramRunListener.onCompleted(r, taskListener);
+
+        verify(telegramApi, times(1)).sendMessage(message);
+        verify(printStream, times(1)).println(notificationLog);
+    }
+
+    @Test
+    public void onStartedFailTest() throws Exception {
+        doThrow(mock(RuntimeException.class))
+                .when(telegramApi).sendMessage(any(String.class));
+
+        String message = "Build " + r.getFullDisplayName() + " has started";
+        String notificationLog = "Error. Telegram notification wasn't sent";
+
+        telegramRunListener.onStarted(r, taskListener);
+
+        verify(telegramApi, times(1)).sendMessage(message);
+        verify(printStream, times(1)).println(notificationLog);
+    }
+
+    @Test
+    public void onCompletedFailTest() throws Exception {
+        doThrow(mock(RuntimeException.class))
+                .when(telegramApi).sendMessage(any(String.class));
+
+        String message = "Build " + r.getFullDisplayName() +
+                " has finished for " + r.getDurationString();
+        String notificationLog = "Error. Telegram notification wasn't sent";
+
+        telegramRunListener.onCompleted(r, taskListener);
+
+        verify(telegramApi, times(1)).sendMessage(message);
+        verify(printStream, times(1)).println(notificationLog);
+    }
+}
