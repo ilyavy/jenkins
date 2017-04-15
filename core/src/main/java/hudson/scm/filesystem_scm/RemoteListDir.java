c5157c017fcd0ed4101a48d9b597bc0d3e7bb0b6
Ilya V <i.voloshanovskiy@innopolis.ru>
2017-04-15 19:28:52 +0300

    filesystem_scm incorporated into Jenkins

---
+++ core/src/main/java/hudson/scm/filesystem_scm/RemoteListDir.java
@@ -0,0 +1,34 @@
+package hudson.scm.filesystem_scm;
+
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.jenkinsci.remoting.RoleChecker;
+
+import hudson.FilePath.FileCallable;
+import hudson.remoting.VirtualChannel;
+
+public class RemoteListDir implements FileCallable< Set<String> > { 
+	
+	private static final long serialVersionUID = 1452212500874165127L;
+
+	public RemoteListDir() {
+	}
+	
+	public Set<String> invoke(File workspace, VirtualChannel channel) throws IOException {
+		Collection<File> list = FileUtils.listFiles(workspace, null, true);
+		Set<String> set = new HashSet<String>();
+		for(File file : list) {
+			String relativePath = FolderDiff.getRelativeName(file.getAbsolutePath(), workspace.getAbsolutePath());
+			set.add(relativePath);
+		}
+		return set;
+	}
+
+    @Override
+    public void checkRoles(RoleChecker checker) throws SecurityException {
+        
+    }
+}
